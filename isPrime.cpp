c6f811e07bf7829f7f35352ba4885266ae29a2be
jamesboi951 <45056053+jamesboi951@users.noreply.github.com>
2019-08-29 13:37:41 -0700

    Create isPrime.cpp

---
+++ isPrime.cpp
@@ -0,0 +1,10 @@
+bool isPrime(int num){
+    bool flag=true;
+    for(int i = 2; i <= num / 2; i++) {
+       if(num % i == 0) {
+          flag = false;
+          break;
+       }
+    }
+    return flag;
+}
